56a39d4f13b9564095811072daccd8cd7c859114
AlePiga <alessandro07.pigaiani@gmail.com>
2025-09-24 11:11:00 +0200

    terzo commit yay

---
+++ src/ApiClient.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiClient {
+    private static HttpClient client = HttpClient.newHttpClient();
+
+    public String fetchQuestion(int amount, int category, String type, String difficulty){
+        String url = "https://opentdb.com/api.php?amount=" + amount + "&category=" + category + "&difficulty=" + difficulty + "&type=" + type;
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("Content-Type", "application/json")
+                .uri(java.net.URI.create(url))
+                .GET()
+                .build();
+        HttpResponse<String> response;
+        try{
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        }
+        catch (IOException | InterruptedException e){
+            return "Errore nella richiesta API!";
+        }
+
+        return response.body();
+    }
+}
